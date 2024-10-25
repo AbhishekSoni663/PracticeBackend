7e8d7ab79267a19cbd173da5e1eee769040d8969
AbhishekSoni663 <Abhisheksoni663@gmail.com>
2024-10-25 23:12:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+app.use(express.json())
+app.use(cors())
+
+app.post("/login", (req, res) => {
+  const { email, pass } = req.body;
+  console.log(req.body);
+  if (!email && !pass) {
+    res.status(404).send("Please provide email and password" + req.body);
+  } else {
+    res.send("Login Success!" + req.body);
+  }
+});
+
+const auth = (req, res, next) => {
+  if (req.headers.authorization === "Abhishek Soni") {
+    next();
+  }else{
+    res.send("Invalid User")
+  }
+};
+
+app.get("/getName", auth, (req, res) => {
+console.log(req.headers.authorization,req.query)
+  res.send({
+    name:"Abhishek Soni",
+  });
+});
+
+
+app.post('/create',(req,res)=>{
+  const {email , pass} = req.body
+  console.log(req.body)
+    if (!email && !pass) {
+      res.status(404).send("Please provide email and password" + req.body);
+    } else {
+      res.send("User With Email" + email + req.body);
+    }
+})
+app.listen(3500, () => {
+    console.log("Server is Running at 3500")
+});
